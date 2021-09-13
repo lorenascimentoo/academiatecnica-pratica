e2223b2838401d55cfecffdf428938fb34ddcee4
lorenascimentoo <lorenanascimento28@gmail.com>
2021-09-13 15:41:51 -0300

    Atividade pratica, modulo 2 - atividade 1

---
+++ src/modulo2/atividadep1/CadastroDeProdutos.java
@@ -0,0 +1,27 @@
+package modulo2.atividadep1;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class CadastroDeProdutos {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+
+        System.out.println("\t============== CADASTRO DE PRODUTOS ==============");
+        System.out.println("\tBem vindo, adicione as informacoes do produto.");
+        System.out.print("\tInforme o nome do produto: ");
+        String nome = sc.next();
+        System.out.print("\tInforme a descrição do produto: ");
+        String desc = sc.next();
+        System.out.print("\tInforme o valor do produto: ");
+        double valor = sc.nextDouble();
+        System.out.println(valor);
+        System.out.print("\tInforme a categoria do produto: ");
+        String categ = sc.next();
+        System.out.println("\t--- PRODUTO CADASTRADO COM SUCESSO! ---");
+        System.out.printf("\tProduto: %s | Descrição: %s | Valor: %.2f | Categoria: %s",nome,desc,valor,categ);
+
+    }
+}
