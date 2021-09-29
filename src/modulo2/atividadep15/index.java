aa8bb2f960024a72481bdb11bd9bb6e01d6c3cb0
lorenascimentoo <lorenanascimento28@gmail.com>
2021-09-29 16:01:43 -0300

    construcao de dois objetos e preenchimento de valores

--- src/modulo2/atividadep15/index.java
+++ src/modulo2/atividadep15/index.java
@@ -1,6 +1,20 @@
 
 public class index {
     public static void main(String[] args) {
-    
+ 
+        //Construcao do primeiro objeto
+        Pessoa pessoa1 = new Pessoa();
+        //Preenchimento dos valores do objeto um
+        pessoa1.nome = "Donna";
+        pessoa1.sobrenome = "Paulsen";
+        pessoa1.idade = 37;
+ 
+        //Construcao do segundo objeto
+        Pessoa pessoa2 = new Pessoa();
+        //Preenchimento dos valores do objeto dois
+        pessoa2.nome = "Harvey";
+        pessoa2.sobrenome = "Specter";
+        pessoa2.idade = 38;
+
     }    
 }
