ea190de49b20c4ff174338f3ac55c526ba44dafd
lorenascimentoo <lorenanascimento28@gmail.com>
2021-10-01 15:35:03 -0300

    inserindo os parametros dos metodos e implementacao dos calculos dos valores

--- src/modulo2/atividadep17/CalculadoraDeImpostos.java
+++ src/modulo2/atividadep17/CalculadoraDeImpostos.java
@@ -4,23 +4,36 @@ public class CalculadoraDeImpostos {
     private double valorIss, valorIof, valorIr;
 
     // metodo utilizado para calculo do ISS
-    public double calcularISS(){
+    public double calcularISS(double valorBase, double aliquota){
         //calculo ISS é o preco do servico * a aliquota praticada
-        valorIss=0;
+        valorIss = valorBase * (aliquota/100);
         return valorIss;
     }
     
     // metodo utilizado para calculo do IOF
-    public double calcularIOF(){
+    public double calcularIOF(double valorBase, double taxaIof){
         //calculo IOF é o valor * a taxa de iof aplicada
-        valorIof=0; 
+        valorIof = valorBase * (taxaIof/100); 
         return valorIof;
     }
     
     // metodo utilizado para calculo do IR
-    public double calcularIR(){
-        //sera realizado a deducao de IR com a base de calculo anual 
-        valorIr=0; 
+    public double calcularIR(double valorBase){
+        //sera realizado a deducao de IR com a base de calculo anual
+        //foi utilizada a tabela de IR do ano base 2020
+        
+        if(valorBase<=22847.76){
+            valorIr = 0;
+        }else if(valorBase > 22847.76 && valorBase <= 33919.80){
+            valorIr = valorBase * (7.5/100);
+        }else if(valorBase > 33919.80 && valorBase <= 45012.60){
+            valorIr = valorBase * (15/100);
+        }else if(valorBase > 45012.60 && valorBase <= 55976.16){
+            valorIr = valorBase * (22.5/100);
+        }else if(valorBase > 55976.16){
+            valorIr = valorBase * (27.5/100);
+        } 
+             
         return valorIr;
     }
 }
