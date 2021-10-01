3bf29618b9827ac11eede69b5e3e30ad7d30f3c2
lorenascimentoo <lorenanascimento28@gmail.com>
2021-10-01 14:56:05 -0300

    criacao dos metodos de calculo de iss, iof e ir

---
+++ src/modulo2/atividadep17/CalculadoraDeImpostos.java
@@ -0,0 +1,18 @@
+
+public class CalculadoraDeImpostos {
+
+    // metodo utilizado para calculo do ISS
+    public double calcularISS(){
+        return 0; //apenas para criacao do metodo
+    }
+    
+    // metodo utilizado para calculo do IOF
+    public double calcularIOF(){
+        return 0; //apenas para criacao do metodo
+    }
+    
+    // metodo utilizado para calculo do IR
+    public double calcularIR(){
+        return 0; //apenas para criacao do metodo
+    }
+}
