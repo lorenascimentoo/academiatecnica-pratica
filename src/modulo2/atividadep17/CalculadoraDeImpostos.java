d830dea1e74ad1360a625b96c06267b4a97a43d9
lorenascimentoo <lorenanascimento28@gmail.com>
2021-10-01 15:22:45 -0300

    declaracao das variaveis privadas que receberao o valor de cada imposto calculado

--- src/modulo2/atividadep17/CalculadoraDeImpostos.java
+++ src/modulo2/atividadep17/CalculadoraDeImpostos.java
@@ -1,18 +1,26 @@
 
 public class CalculadoraDeImpostos {
 
+    private double valorIss, valorIof, valorIr;
+
     // metodo utilizado para calculo do ISS
     public double calcularISS(){
-        return 0; //apenas para criacao do metodo
+        //calculo ISS é o preco do servico * a aliquota praticada
+        valorIss=0;
+        return valorIss;
     }
     
     // metodo utilizado para calculo do IOF
     public double calcularIOF(){
-        return 0; //apenas para criacao do metodo
+        //calculo IOF é o valor * a taxa de iof aplicada
+        valorIof=0; 
+        return valorIof;
     }
     
     // metodo utilizado para calculo do IR
     public double calcularIR(){
-        return 0; //apenas para criacao do metodo
+        //sera realizado a deducao de IR com a base de calculo anual 
+        valorIr=0; 
+        return valorIr;
     }
 }
