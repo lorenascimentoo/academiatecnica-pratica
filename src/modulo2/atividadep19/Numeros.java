fd3199d5c27d5e91a56da9df96539109e26ac35c
lorenascimentoo <lorenanascimento28@gmail.com>
2021-10-04 17:23:33 -0300

    atribuindo valores em cada posicao do array

--- src/modulo2/atividadep19/Numeros.java
+++ src/modulo2/atividadep19/Numeros.java
@@ -3,5 +3,16 @@ public class Numeros {
         // Criando um array inteiro com 10 posicoes
         int[] numeros = new int[10];
 
+        // Atribuindo valores em cada posicao do array
+        numeros[0] = 1;
+        numeros[1] = 2;
+        numeros[2] = 3;
+        numeros[3] = 4;
+        numeros[4] = 5;
+        numeros[5] = 6;
+        numeros[6] = 7;
+        numeros[7] = 8;
+        numeros[8] = 9;
+        numeros[9] = 10;
     }
 }
