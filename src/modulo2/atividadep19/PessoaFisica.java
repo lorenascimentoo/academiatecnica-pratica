a79ab69558b2399ae7ce836acf16f385f70cb7b7
lorenascimentoo <lorenanascimento28@gmail.com>
2021-10-04 17:39:02 -0300

    implementacao dos metodos getters e setters dos atributos

--- src/modulo2/atividadep19/PessoaFisica.java
+++ src/modulo2/atividadep19/PessoaFisica.java
@@ -6,4 +6,53 @@ public class PessoaFisica {
     private Byte idade;
     private String rg;
     private String cpf;
+
+    //metodo de acesso ao atributo codigo
+    public int getCodigo() {
+        return codigo;
+    }
+    //metodo de modificacao do atributo codigo
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    //metodo de acesso ao atributo nome
+    public String getNome() {
+        return nome;
+    }
+    //metodo de modificacao do atributo nome
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    //metodo de acesso ao atributo sobrenome
+    public String getSobrenome() {
+        return sobrenome;
+    }
+    //metodo de modificacao do atributo sobrenome
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+    //metodo de acesso ao atributo idade
+    public Byte getIdade() {
+        return idade;
+    }
+    //metodo de modificacao do atributo idade
+    public void setIdade(Byte idade) {
+        this.idade = idade;
+    }
+    //metodo de acesso ao atributo rg
+    public String getRg() {
+        return rg;
+    }
+    //metodo de modificacao do atributo rg
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+    //metodo de acesso ao atributo cpf
+    public String getCpf() {
+        return cpf;
+    }
+    //metodo de modificacao do atributo cpf
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
