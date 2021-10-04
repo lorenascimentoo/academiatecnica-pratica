a11c2102912add2fd57b41c16b53f7c2fbaa0a9c
Lorena Nascimento <lorenanascimento28@gmail.com>
2021-10-04 14:20:28 -0300

    chamando os metodos gettes e setters do objeto e imprimindo os dados

--- src/modulo2/atividadep18/TelaPrincipalBanco.java
+++ src/modulo2/atividadep18/TelaPrincipalBanco.java
@@ -1,7 +1,20 @@
 public class TelaPrincipalBanco {
 
     public static void main(String[] args) {
-        
-    }
+        //Instanciando um novo objeto do tipo conta corrente
+    ContaCorrente contaC = new ContaCorrente();
 
+    // modificando os valores do atributos por meio dos metodos set
+    contaC.setAgencia(1234);
+    contaC.setNumero(1001);
+    contaC.setCodCliente(10);
+    contaC.setSaldo(100);
+    
+    //Acessando os valores atraves dos metodos get para impressao dos resutados
+    System.out.println("========== CONTA CORRENTE ==========");
+    System.out.println("-- CLIENTE: "+contaC.getCodCliente());
+    System.out.printf("-- AGENCIA: %d || CONTA: %d\n",contaC.getAgencia(), contaC.getNumero());
+    System.out.printf("-- SALDO: %.2f\n",contaC.getSaldo());
+    System.out.println("====================================");
+    }
 }
