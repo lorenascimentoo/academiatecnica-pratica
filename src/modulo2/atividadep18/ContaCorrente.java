475cdfb7b2805f23d38c86e5c5255b6f988b8b54
Lorena Nascimento <lorenanascimento28@gmail.com>
2021-10-04 13:44:14 -0300

    criando os metodos getters e setters dos atributos

--- src/modulo2/atividadep18/ContaCorrente.java
+++ src/modulo2/atividadep18/ContaCorrente.java
@@ -1,6 +1,41 @@
 public class ContaCorrente {
+    // fazendo o encapsulamento dos atributos
+    // tornando-os privados
     private int agencia;
     private int numero;
     private int codCliente;
     private double saldo;
+ 
+    //metodo para acessar o atributo agencia
+    public int getAgencia(){
+        return this.agencia;
+    }
+    //metodo para modificar o atributo agencia
+    public void setAgencia(int agencia){
+        this.agencia = agencia;
+    }
+    //metodo para acessar o atributo numero
+    public int getNumero(){
+        return this.numero;
+    }
+    //metodo para modificar o atributo numero
+    public void setNumero(int numero){
+        this.numero = numero;
+    }
+    //metodo para acessar o atributo codCliente
+    public int getCodCliente(){
+        return this.codCliente;
+    }
+    //metodo para modificar o atributo codCliente
+    public void setCodCliente(int codCliente){
+        this.codCliente = codCliente;
+    }
+    //metodo para acessar o atributo saldo
+    public double getSaldo(){
+        return this.saldo;
+    }
+    //metodo para modificar o atributo saldo
+    public void setSaldo(int saldo){
+        this.saldo = saldo;
+    }
 }
