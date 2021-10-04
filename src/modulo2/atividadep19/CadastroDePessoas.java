c0cd75d025c7f8fd1d8236e271138cd8af28b6ca
lorenascimentoo <lorenanascimento28@gmail.com>
2021-10-04 17:44:35 -0300

    criacao de um array do tipo pessoafisica com 5 posicoes

--- src/modulo2/atividadep19/CadastroDePessoas.java
+++ src/modulo2/atividadep19/CadastroDePessoas.java
@@ -1,5 +1,7 @@
 public class CadastroDePessoas {
     public static void main(String[] args) {
         
+        //Criando um array do tipo pessoa com 5 posicoes
+        PessoaFisica[] pessoasFisicas = new PessoaFisica[5];
     }
 }
