a974c698e325a974df1db1e5beeb213cf5a55096
lorenascimentoo <lorenanascimento28@gmail.com>
2021-10-05 15:33:27 -0300

    classe aluno herdando a classe pessoa

--- src/modulo2/atividadep20/Aluno.java
+++ src/modulo2/atividadep20/Aluno.java
@@ -1,4 +1,6 @@
-public class Aluno {
+// aluno herdando as carateristicas de pessoa
+// em java a palavra reservada extends indica heranca
+public class Aluno extends Pessoa{
     String curso;
     String turma;
 }
