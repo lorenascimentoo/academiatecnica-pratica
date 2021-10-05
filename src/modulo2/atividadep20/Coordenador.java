c6fa704f94850179e7be5c5a87e2772a49705fd2
lorenascimentoo <lorenanascimento28@gmail.com>
2021-10-05 15:31:24 -0300

    classe coordenador herdando a classe funcionario

--- src/modulo2/atividadep20/Coordenador.java
+++ src/modulo2/atividadep20/Coordenador.java
@@ -1,4 +1,6 @@
-public class Coordenador {
+// coordenador herdando as carateristicas de funcionario
+// em java a palavra reservada extends indica heranca
+public class Coordenador extends Funcionario{
     String curso_coordenado;
     double bonus;
 }
