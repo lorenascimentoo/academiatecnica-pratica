43b10c1cb632b370f512fb59a01c4bc4770fbd56
lorenascimentoo <lorenanascimento28@gmail.com>
2021-10-05 15:30:19 -0300

    classe funcionario herdando a classe pessoa

--- src/modulo2/atividadep20/Funcionario.java
+++ src/modulo2/atividadep20/Funcionario.java
@@ -1,4 +1,6 @@
-public class Funcionario {
+// funcionario herdando as carateristicas de pessoa
+// em java a palavra reservada extends indica heranca
+public class Funcionario extends Pessoa{
     double salario;
     String departamento;
 }
