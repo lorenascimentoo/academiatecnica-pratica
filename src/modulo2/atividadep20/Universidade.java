d67de34f01b66125ebce8cb0b20b52cb03bf9fde
lorenascimentoo <lorenanascimento28@gmail.com>
2021-10-05 15:48:44 -0300

    criacao da classe universidade, implementacao do metodo main e criacao dos objetos tipo aluno, coordenador e funcionario

---
+++ src/modulo2/atividadep20/Universidade.java
@@ -0,0 +1,11 @@
+public class Universidade {
+    
+    public static void main(String[] args) {
+        // Criando uma instancia de um objeto do tipo funcionario
+        Funcionario f = new Funcionario();
+        // Criando uma instancia de um objeto do tipo coordenador
+        Coordenador c = new Coordenador();
+        // Criando uma instancia de um objeto do tipo Aluno
+        Aluno a = new Aluno();
+    }
+}
