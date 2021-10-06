b9cdb9ef175dd11b29f76af24f8a98a2e58e0ae9
lorenascimentoo <lorenanascimento28@gmail.com>
2021-10-06 13:28:48 -0300

    implementando heranca na classe conta corrente

--- src/modulo2/atividadep21/ContaCorrente.java
+++ src/modulo2/atividadep21/ContaCorrente.java
@@ -1,4 +1,7 @@
-public class ContaCorrente {
+// a palavra chave extends é utilizava para realizar a heranca de uma classe
+// conta corrente herda conta
+public class ContaCorrente extends Conta{
+    // atributos encapsulados
     private double taxa_transferencia = 10.50;
     private double taxa_manutencao = 2.50;
     private byte numero_transferencias = 0;
