a6cb7ee770a586a42b21c1b08243b7f50c769d89
lorenascimentoo <lorenanascimento28@gmail.com>
2021-10-06 13:21:40 -0300

    criacao dos metodos getters e setters dos atributos da classe conta

--- src/modulo2/atividadep21/Conta.java
+++ src/modulo2/atividadep21/Conta.java
@@ -1,6 +1,22 @@
 
 public class Conta {
+    //atributos encapsulados
     private int cod_cliente;
     private double saldo;
-   
+    
+    // metodos getters para acesso aos atributos
+    // metodo setters para atribuicao dos atributos
+    public void setCod_cliente(int cod_cliente) {
+        this.cod_cliente = cod_cliente;
+    }
+    public int getCod_cliente() {
+        return cod_cliente;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
 }
