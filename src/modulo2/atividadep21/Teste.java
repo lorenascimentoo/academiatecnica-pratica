e682651e90176e4110af77e15020db9cc848f823
lorenascimentoo <lorenanascimento28@gmail.com>
2021-10-06 13:41:02 -0300

    atribuindo os valores de saldo e cod cliente com os metodos setters

--- src/modulo2/atividadep21/Teste.java
+++ src/modulo2/atividadep21/Teste.java
@@ -2,7 +2,15 @@ public class Teste {
     public static void main(String[] args) {
         // inicializacao de um novo objeto do tipo Conta
         Conta c = new Conta();
+        // utilizando os metodos setters para atribuicao dos valores dos atributos da classe conta
+        c.setCod_cliente(1);
+        c.setSaldo(100);
+
         // inicializacao de um novo objeto do tipo Conta Corrente
         ContaCorrente cc = new ContaCorrente();
+        // por heranca, a classe conta corrente recebe os atributos da classe conta
+        // utilizando os metodos setters para atribuicao dos valores dos atributos
+        cc.setCod_cliente(2);
+        cc.setSaldo(200);
     }
 }
