eff40be6bbae65c0fd01cc0581f9f190f9ded62f
lorenascimentoo <lorenanascimento28@gmail.com>
2021-10-06 13:37:08 -0300

    criacao da classe teste, implementacao do metodo main e inicializacao dos objetos

---
+++ src/modulo2/atividadep21/Teste.java
@@ -0,0 +1,8 @@
+public class Teste {
+    public static void main(String[] args) {
+        // inicializacao de um novo objeto do tipo Conta
+        Conta c = new Conta();
+        // inicializacao de um novo objeto do tipo Conta Corrente
+        ContaCorrente cc = new ContaCorrente();
+    }
+}
