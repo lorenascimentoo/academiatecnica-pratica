f080c87bc079b142a7a024fbf45f9220794447b7
lorenascimentoo <lorenanascimento28@gmail.com>
2021-10-06 13:25:55 -0300

    atribuicao de valor padrao aos atributos da classe conta corrente

--- src/modulo2/atividadep21/ContaCorrente.java
+++ src/modulo2/atividadep21/ContaCorrente.java
@@ -1,5 +1,5 @@
 public class ContaCorrente {
-    private double taxa_transferencia;
-    private double taxa_manutencao;
-    private byte numero_transferencias;
+    private double taxa_transferencia = 10.50;
+    private double taxa_manutencao = 2.50;
+    private byte numero_transferencias = 0;
 }
