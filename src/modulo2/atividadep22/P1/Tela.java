1911cbd4f1dbffe9e3cee4175db82e94076919e5
lorenascimentoo <lorenanascimento28@gmail.com>
2021-10-07 16:13:45 -0300

    instanciacao de dois objetos do tipo aluno, atribuicao dos valores por meio dos metodos setters e impressao dos dados

--- src/modulo2/atividadep22/P1/Tela.java
+++ src/modulo2/atividadep22/P1/Tela.java
@@ -3,5 +3,28 @@ package P1;
 public class Tela {
     public static void main(String[] args) {
         
+        // Instanciando um objeto do tipo aluno
+        Aluno a1 = new Aluno();
+        // atribuindo os valores com os metodos setters
+        a1.setNome_completo("Natasha Zapata");
+        a1.setIdade((byte)18);
+        a1.setMatricula(1);
+
+        // Instanciando um objeto do tipo aluno
+        Aluno a2 = new Aluno();
+        // atribuindo os valores com os metodos setters
+        a2.setNome_completo("Rich DotCom");
+        a2.setIdade((byte)30);
+        a2.setMatricula(10211);
+
+        System.out.println("============= DADOS DO ALUNO =============");
+        System.out.println(a1);
+        System.out.println("==========================================");
+        
+        System.out.println();
+        
+        System.out.println("============= DADOS DO ALUNO =============");
+        System.out.println(a2);
+        System.out.println("==========================================");
     }
 }
