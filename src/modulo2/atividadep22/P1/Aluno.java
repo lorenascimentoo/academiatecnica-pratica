339129424bdd1f82464741b76cf75ac48470674e
lorenascimentoo <lorenanascimento28@gmail.com>
2021-10-07 15:58:22 -0300

    implementacao dos metodos getters e setters dos atributos da classe aluno

--- src/modulo2/atividadep22/P1/Aluno.java
+++ src/modulo2/atividadep22/P1/Aluno.java
@@ -5,5 +5,30 @@ public class Aluno {
     private String nome_completo;
     private byte idade;
     private int matricula;
+
+    // METODOS MODIFICADORES
+    // getters para acesso dos atributos
+    // setter para atribuicao de valores aos atributos
+
+    public String getNome_completo() {
+        return nome_completo;
+    }
+    public void setNome_completo(String nome_completo) {
+        this.nome_completo = nome_completo;
+    }
+
+    public byte getIdade() {
+        return idade;
+    }
+    public void setIdade(byte idade) {
+        this.idade = idade;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
     
 }
