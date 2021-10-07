de7e25f2ca8bcf6d7f06efbe403302ad46063fa8
lorenascimentoo <lorenanascimento28@gmail.com>
2021-10-07 16:24:26 -0300

    implementacao do metodo construtor

--- src/modulo2/atividadep22/P2/ContaCorrente.java
+++ src/modulo2/atividadep22/P2/ContaCorrente.java
@@ -4,5 +4,12 @@ public class ContaCorrente {
     // atributos encapsulados
     private double saldo;
     private double taxa_saque;
-    private double codigo_cliente;
+    private int codigo_cliente;
+
+    // metodo construtor que recebe o saldo e o codigo do cliente
+    // e atribui aos atributos da classe
+    public ContaCorrente(double saldo, int codigo_cliente){
+        this.saldo = saldo;
+        this.codigo_cliente = codigo_cliente;
+    }
 }
