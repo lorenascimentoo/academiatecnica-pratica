9d87b61ad5c13e06f8deefafadfcff70e10a8c61
lorenascimentoo <lorenanascimento28@gmail.com>
2021-10-07 16:55:26 -0300

    implementacao de metodo que recebe dois inteiros, soma e retorna inteiro

--- src/modulo2/atividadep22/P3/Calculadora.java
+++ src/modulo2/atividadep22/P3/Calculadora.java
@@ -1,5 +1,9 @@
 package P3;
 
 public class Calculadora {
-     
+
+    // metodo que recebe dois inteiros soma e retorna como inteiro
+    public int soma(int num1, int num2){
+        return num1+num2;
+    }
 }
