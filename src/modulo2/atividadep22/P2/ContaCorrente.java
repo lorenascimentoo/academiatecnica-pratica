921a23789903e7f7519c9d9af5af8b0f5c01ee1d
lorenascimentoo <lorenanascimento28@gmail.com>
2021-10-07 16:31:44 -0300

    atribuindo um valor fixo ao atributo de taxa de saque

--- src/modulo2/atividadep22/P2/ContaCorrente.java
+++ src/modulo2/atividadep22/P2/ContaCorrente.java
@@ -3,7 +3,7 @@ package P2;
 public class ContaCorrente {
     // atributos encapsulados
     private double saldo;
-    private double taxa_saque;
+    private double taxa_saque = 3.82;
     private int codigo_cliente;
 
     // metodo construtor que recebe o saldo e o codigo do cliente
