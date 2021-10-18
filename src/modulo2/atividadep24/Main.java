18486c28310970ec0994b09f4dea55a1bc314cf5
lorenascimentoo <lorenanascimento28@gmail.com>
2021-10-18 14:14:34 -0300

    criacao de um objeto do tipo veiculo e um do tipo carro e atribuicao de valores aos objetos

--- src/modulo2/atividadep24/Main.java
+++ src/modulo2/atividadep24/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         
+        // instanciando um objeto do tipo veiculo
+        Veiculo veiculo1 = new Veiculo();
+        // atribuindo valores aos atributos do objeto do tipo veiculo
+        veiculo1.marca = "Fiat";
+        veiculo1.modelo = "MOBI";
+        veiculo1.placa = "XD41C44";
+
+        // instanciando um objeto do tipo carro
+        Carro carro1 = new Carro();
+        // atribuindo valores aos atributos do objeto do tipo carro
+        carro1.capacidade_portamalas = "30L";
+        carro1.numero_portas = 4;
     }
 }
