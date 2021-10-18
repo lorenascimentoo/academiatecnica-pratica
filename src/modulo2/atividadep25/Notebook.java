44cd9b6f2028d8ec2d45a662046dc91964f75933
lorenascimentoo <lorenanascimento28@gmail.com>
2021-10-18 16:37:31 -0300

    criacao da classe notebook herdando de computador e seus atributos

---
+++ src/modulo2/atividadep25/Notebook.java
@@ -0,0 +1,6 @@
+// a classe notebook herda os atributos da classe computador
+public class Notebook extends Computador{
+    public String bateria;
+    public String velocidade_wifi;
+    public String versao_bluetooth;
+}
