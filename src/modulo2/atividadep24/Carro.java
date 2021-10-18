55a0f37e29afad4071117f93598922bdb05b9313
lorenascimentoo <lorenanascimento28@gmail.com>
2021-10-18 14:33:03 -0300

    implementando o conceito de heranca, atribuicao de valores as propriedades herdadas e impressao dos dados

--- src/modulo2/atividadep24/Carro.java
+++ src/modulo2/atividadep24/Carro.java
@@ -1,4 +1,5 @@
-public class Carro {
+// a classe carro herda todos os atributos da classe veiculo
+public class Carro extends Veiculo{
     public byte numero_portas;
     public String capacidade_portamalas;
 }
--- src/modulo2/atividadep24/Main.java
+++ src/modulo2/atividadep24/Main.java
@@ -13,6 +13,10 @@ public class Main {
         // atribuindo valores aos atributos do objeto do tipo carro
         carro1.capacidade_portamalas = "30L";
         carro1.numero_portas = 4;
+        // atribuindo valores aos atributos herdados da classe veiculo
+        carro1.marca = "Renault";
+        carro1.modelo = "KWID";
+        carro1.placa = "41DC14F";
 
         //Criacao de um object a partir do objeto da classe veiculo
         Object objV = veiculo1;
@@ -30,8 +34,13 @@ public class Main {
         System.out.println("Marca: "+veiculoObj.marca);
         System.out.println("Modelo: "+veiculoObj.modelo);
         System.out.println("Placa: "+veiculoObj.placa);
-
+        System.out.println();
+        System.out.println("--");
+        System.out.println();
         // impressao dos dados do objeto carro convertido
+        System.out.println("Marca: "+carroObj.marca);
+        System.out.println("Modelo: "+carroObj.modelo);
+        System.out.println("Placa: "+carroObj.placa);
         System.out.println("Numero de portas: "+carroObj.numero_portas);
         System.out.println("Capacidade porta-malas: "+carroObj.capacidade_portamalas);
     }
