4030ef5497b8fa9c47ab3d22ba929af507ac20cd
lorenascimentoo <lorenanascimento28@gmail.com>
2021-10-19 15:40:25 -0300

    implementacao do metodo para remover um item do array

--- src/modulo2/atividadep26/Dados.java
+++ src/modulo2/atividadep26/Dados.java
@@ -14,6 +14,8 @@ public class Dados {
         if(posicao < this.dados.length){
             // caso tenha, ele adiciona o objeto a posicao do array
             this.dados[posicao] = obj;
+            // após inserir, incrementa a posicao para quando for adicionar um novo obj
+            posicao++;
         // caso nao haja espaço
         } else {
             // instancia um novo objeto com o dobro do espaço
@@ -26,9 +28,10 @@ public class Dados {
 
             // após o backup dos dados, adiciona o obj a posicao - agora existente
             maisDados[posicao] = obj;
-
             // com os dados atualizados, o novo vetor é atribuido ao vetor da classe, tendo um novo espaco em memoria e maior valor 
             this.dados = maisDados;
+            // após inserir, incrementa a posicao para quando for adicionar um novo obj
+            posicao++;
         }
     }
 
@@ -36,5 +39,27 @@ public class Dados {
     public int size(){
         // retorna o valor da posicao atual do array mais 1, dado que inicia em zero
         return posicao+1;
+    }
+
+    // metodo para remover dados do array
+    // recebe um tipo object para aceitar qualquer tipo de objeto
+    public void remove(Object obj){
+        // percorre todos os dados do array 
+        for (int i = 0; i < this.dados.length; i++) {
+            // verifica se o objeto no array dado é o mesmo obj recebido no metodo
+            if (this.dados[i].equals(obj)) {
+                // caso seja o mesmo objeto, ele reorganiza o vetor
+                // iniciando a varredura na posicao do objeto a ser removido    
+                for( int j = i; i<this.dados.length; j++){
+                    // atribui os dados da posicao posterior para a posicao anterior
+                    // ex dados[4] = dados[5]
+                    this.dados[j] = this.dados[j+1];
+                }
+                // decrementa a posicao visto que foi removido um item
+                posicao --;
+                // limpa os dados da ultima posicao inserida
+                this.dados[posicao]=null;
+            }
+        }
     }
 }
