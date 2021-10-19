8e0fe2e6126dd1bbe894d8d9953a48e4d1ffa812
lorenascimentoo <lorenanascimento28@gmail.com>
2021-10-19 14:47:53 -0300

    sobrescrita do metodo equals nas classes aluno e pessoa

--- src/modulo2/atividadep26/Aluno.java
+++ src/modulo2/atividadep26/Aluno.java
@@ -3,4 +3,18 @@ public class Aluno extends Pessoa{
     public String matricula;
     public String turma;
     public String curso;
+
+    @Override
+    public boolean equals(Object obj) {
+        // converte o object recebido em um objeto do tipo aluno
+        Aluno aVerificado = (Aluno) obj;
+        // verifica se todos os dados do objeto são iguais
+        // as strings são verificadas pelo metodo equals da classe string
+        // por herdar da classe pessoa, e necessario chamar o metodo da mae para verificar os dados herdados
+        if(super.equals(aVerificado) && this.matricula.equals(aVerificado.matricula) && this.turma.equals(aVerificado.turma) && this.curso.equals(aVerificado.curso)){
+            return true;
+        }
+        // caso nao seja igual retorna false
+        return false;
+    }
 }
--- src/modulo2/atividadep26/Pessoa.java
+++ src/modulo2/atividadep26/Pessoa.java
@@ -2,6 +2,20 @@ public class Pessoa {
 
     public String nome;
     public String sobrenome;
-    public byte idade;
+    public int idade;
     
+    @Override
+    public boolean equals(Object obj) {
+        // converte o object recebido em um objeto do tipo pessoa
+        Pessoa pVerificada = (Pessoa) obj;
+        // verifica se todos os dados do objeto são iguais
+        // as strings são verificadas pelo metodo equals da classe string
+        // a classe byte não possui o metodo equals, assim foi feita com o operador de comparacao ==
+        if(this.nome.equals(pVerificada.nome) && this.sobrenome.equals(pVerificada.sobrenome) && this.idade == pVerificada.idade){
+            return true;
+        }
+
+        // caso nao seja igual retorna false
+        return false;
+    }
 }
