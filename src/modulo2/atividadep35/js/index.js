676d4a04e474beb40b1a4916b6c79b647e327859
lorenascimentoo <lorenanascimento28@gmail.com>
2021-11-03 11:06:50 -0300

    implementacao da página index: html, css e js

---
+++ src/modulo2/atividadep35/js/index.js
@@ -0,0 +1,19 @@
+function resumo(){
+    let frame = document.getElementById("frame");
+    frame.src="resumo.html"
+}
+
+function experiencias(){
+    let frame = document.getElementById("frame");
+    frame.src="resumo.html"
+}
+
+function habilidades(){
+    let frame = document.getElementById("frame");
+    frame.src="resumo.html"
+}
+
+function contatos(){
+    let frame = document.getElementById("frame");
+    frame.src="resumo.html"
+}
