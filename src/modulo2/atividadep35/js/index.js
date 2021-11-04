bdeb0a6e9939387f051f1bbdea24e5a27f61e3db
lorenascimentoo <lorenanascimento28@gmail.com>
2021-11-04 19:34:00 -0300

    alteracoes na index e implementacao da pagina inicial

--- src/modulo2/atividadep35/js/index.js
+++ src/modulo2/atividadep35/js/index.js
@@ -5,15 +5,20 @@ function resumo(){
 
 function experiencias(){
     let frame = document.getElementById("frame");
-    frame.src="resumo.html"
+    frame.src="experiencias.html"
 }
 
 function habilidades(){
     let frame = document.getElementById("frame");
-    frame.src="resumo.html"
+    frame.src="habilidades.html"
 }
 
 function contatos(){
     let frame = document.getElementById("frame");
-    frame.src="resumo.html"
+    frame.src="contatos.html"
+}
+
+function hello(){
+    let frame = document.getElementById("frame");
+    frame.src="helloworld.html"
 }
