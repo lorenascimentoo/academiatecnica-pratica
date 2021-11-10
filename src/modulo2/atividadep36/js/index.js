80717a8cfee1ecd6deb8514093936c76b36d624d
lorenascimentoo <lorenanascimento28@gmail.com>
2021-11-10 19:44:00 -0300

    atp36 - atualizacao da pagina inicial

--- src/modulo2/atividadep36/js/index.js
+++ src/modulo2/atividadep36/js/index.js
@@ -1,7 +1,31 @@
-function inicializa(event){
+function inicializa(event) {
     event.preventDefault();
-    var usuarios=[];
-    localStorage.setItem("usuarios",JSON.stringify(usuarios));
+    var usuarios = [];
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
     alert("SISTEMA PRONTO PARA USO!");
 }
 
+function cadastrar() {
+    let frame = document.getElementById("frame");
+    frame.src = "cadastrar.html";
+}
+
+function listar() {
+    let frame = document.getElementById("frame");
+    frame.src = "listar.html";
+}
+
+function alterar() {
+    let frame = document.getElementById("frame");
+    frame.src = "alterar.html";
+}
+
+function deletar() {
+    let frame = document.getElementById("frame");
+    frame.src = "deletar.html";
+}
+
+function inicio(){
+    let frame = document.getElementById("frame");
+    frame.src = "welcome.html";
+}
