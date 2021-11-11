3c2e192727fcc2b5ce4b24839489359ea687b99f
lorenascimentoo <lorenanascimento28@gmail.com>
2021-11-11 17:29:24 -0300

    listando as categorias do BD

---
+++ src/modulo2/atividadep47/Read.java
@@ -0,0 +1,31 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Read {
+    public static void main(String[] args) {
+        ConexaoBD conecta = new ConexaoBD();
+
+        try {
+            Connection conn = conecta.start("postgres", "aula2021");
+
+            Statement statement = conn.createStatement();
+            
+            String sql = "SELECT ID, NOME FROM CATEGORIA;";
+            statement.execute(sql);
+
+            ResultSet retorno = statement.getResultSet();
+
+            while(retorno.next()){
+                int id = retorno.getInt("ID");
+                String nome = retorno.getString("NOME");
+
+                System.out.printf("== ID: %d\n== NOME: %s\n", id, nome);
+            }
+        } catch (SQLException e) {
+            
+            System.out.println("Não foi possível conectar ao banco de dados");
+        }
+    }
+}
