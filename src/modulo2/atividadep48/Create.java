730f8e3166cc5d310e2aa0e4cce21a5706288049
lorenascimentoo <lorenanascimento28@gmail.com>
2021-11-12 11:52:49 -0300

    alteracao da classe de cadastro de categorias para PreparedStatement

--- src/modulo2/atividadep48/Create.java
+++ src/modulo2/atividadep48/Create.java
@@ -1,4 +1,5 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -10,9 +11,14 @@ public class Create {
         try {
             Connection conn = conecta.start("postgres", "aula2021");
 
-            Statement statement = conn.createStatement();
-            String sql = "insert into categoria(nome,descricao)values('audiovisual','bla');";
-            statement.execute(sql, Statement.RETURN_GENERATED_KEYS);
+            String nome = "papelaria";
+            String descricao = "escola,escritorio";
+            String sql = "insert into categoria(nome,descricao)values(?,?);";
+            
+            PreparedStatement statement = conn.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1,nome);
+            statement.setString(2,descricao);
+            statement.execute();
 
             ResultSet ids = statement.getGeneratedKeys();
 
