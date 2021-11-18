08a65419394f70e2bb89e4074c19443d78d7948b
lorenascimentoo <lorenanascimento28@gmail.com>
2021-11-18 11:55:59 -0300

    criacao do projeto maven com eclipse

---
+++ src/modulo2/atividadep52/src/main/java/com/iamlorxn/atp/atividadep52/App.java
@@ -0,0 +1,13 @@
+package com.iamlorxn.atp.atividadep52;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
