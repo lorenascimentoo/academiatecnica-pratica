07bd837d9a4018e868e8e3c15784f7211acdb957
lorenascimentoo <lorenanascimento28@gmail.com>
2021-11-22 11:38:46 -0300

    implementando a camada dao

---
+++ src/modulo2/atividadep52/src/main/java/com/iamlorxn/atp/atividadep52/dao/CategoriaDao.java
@@ -0,0 +1,102 @@
+package com.iamlorxn.atp.atividadep52.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import com.iamlorxn.atp.atividadep52.model.Categoria;
+
+public class CategoriaDao {
+
+	public int create(Categoria model) {
+		int idGerado = 0;
+
+		try (Connection conn = new ConnectionFactory().getConnection()) {
+			String sql = "insert into categoria(nome,descricao)values(?,?);";
+			try {
+				PreparedStatement statement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+				statement.setString(1, model.getNome());
+				statement.setString(2, model.getDescricao());
+				statement.execute();
+
+				ResultSet ids = statement.getGeneratedKeys();
+
+				while (ids.next()) {
+					idGerado = ids.getInt("id");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return idGerado;
+	}
+
+	public ArrayList<Categoria> read() {
+		ArrayList<Categoria> lista = new ArrayList<Categoria>();
+		try (Connection conn = new ConnectionFactory().getConnection()) {
+
+			String sql = "SELECT * FROM CATEGORIA;";
+			try {
+				PreparedStatement statement = conn.prepareStatement(sql);
+				statement.execute();
+				ResultSet retorno = statement.getResultSet();
+
+				while (retorno.next()) {
+					Categoria cat = new Categoria(retorno.getInt("id"), retorno.getString("nome"),
+							retorno.getString("descricao"));
+					lista.add(cat);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return lista;
+	}
+
+	public int update(Categoria model) {
+		int qntAlterada = 0;
+		try (Connection conn = new ConnectionFactory().getConnection()) {
+			String sql = "UPDATE CATEGORIA SET NOME=?,DESCRICAO=? WHERE ID=?";
+
+			try {
+				PreparedStatement statement = conn.prepareStatement(sql);
+				statement.setString(1, model.getNome());
+				statement.setString(2, model.getDescricao());
+				statement.setInt(3, model.getId());
+				statement.execute();
+				qntAlterada = statement.getUpdateCount();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return qntAlterada;
+	}
+
+	public int delete(int id) {
+		int qntAlterada = 0;
+		try (Connection conn = new ConnectionFactory().getConnection()) {
+			String sql = "DELETE FROM CATEGORIA WHERE ID=?";
+
+			try {
+				PreparedStatement statement = conn.prepareStatement(sql);
+				statement.setInt(1, id);
+				statement.execute();
+				qntAlterada = statement.getUpdateCount();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return qntAlterada;
+	}
+}
---
+++ src/modulo2/atividadep52/src/main/java/com/iamlorxn/atp/atividadep52/dao/ConnectionFactory.java
@@ -0,0 +1,52 @@
+package com.iamlorxn.atp.atividadep52.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class ConnectionFactory {
+	private String driverType;
+    private String driverName;
+    private String host;
+    private int port;
+    private String database;
+    private String user;
+    private String pwd;
+
+    private DataSource dataSource;
+
+    public ConnectionFactory(){
+        //informacoes da conexao
+        String connecString = this.getUrl();
+        this.user = "postgres";
+        this.pwd = "aula2021";
+
+        // pool
+        ComboPooledDataSource pool = new ComboPooledDataSource();
+        pool.setJdbcUrl(connecString);
+        pool.setUser(this.user);
+        pool.setPassword(this.pwd);
+        pool.setMaxPoolSize(50);
+        
+        this.dataSource = pool;
+    }
+
+    //gera a exceção no método para que seja tratada quando a conexao for iniciada
+    public Connection getConnection() throws SQLException{
+        return this.dataSource.getConnection();
+    }
+    //gera a url para a conexao   
+    private String getUrl(){
+        //informacoes da url
+        this.driverType="jdbc";
+        this.driverName="postgresql";
+        this.host="localhost";
+        this.port=5432;
+        this.database="postgres";
+        // retorna a string que compoe a url
+        return String.format("%s:%s://%s:%d/%s", this.driverType,this.driverName,this.host,this.port,this.database);
+    }
+}
