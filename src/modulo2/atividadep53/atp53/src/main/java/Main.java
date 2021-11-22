4b0f38b5ec8e75f2aa89f57a7983a7803c1a188a
lorenascimentoo <lorenanascimento28@gmail.com>
2021-11-22 11:59:01 -0300

    projeto base atp52

---
+++ src/modulo2/atividadep53/atp53/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+import view.CategoriaDesktopView;
+
+public class Main {
+    public static void main(String[] args) {
+        CategoriaDesktopView vc2 = new CategoriaDesktopView();
+        vc2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
