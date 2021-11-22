73c0df6aa1da5ac1537c41c3674f893e1b99a134
lorenascimentoo <lorenanascimento28@gmail.com>
2021-11-22 15:01:08 -0300

    leitura de parametros para produto

--- src/modulo2/atividadep53/atp53/src/main/java/servlet/Produto.java
+++ src/modulo2/atividadep53/atp53/src/main/java/servlet/Produto.java
@@ -12,7 +12,18 @@ import jakarta.servlet.http.HttpServletResponse;
 public class Produto extends HttpServlet{
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String nome = req.getParameter("nome");
+        String parametroIdCat = req.getParameter("id_categoria");
+
         PrintWriter out = resp.getWriter();
         out.println("Modulo Produto");
+
+        if (parametroIdCat != null ) {
+            int id_categoria = Integer.parseInt(parametroIdCat);
+            out.printf("== PRODUTO: %s \n== ID_CATEGORIA: %d",nome,id_categoria);
+        } else{
+            out.printf("== PRODUTO: %s",nome);
+        }
+        
     }
 }
