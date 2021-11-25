35eb995b59cf5fd5655e87d6e9d6360b215d3d34
lorenascimentoo <lorenanascimento28@gmail.com>
2021-11-25 16:46:20 -0300

    fazendo uma insercao e leitura de categorias com JPA

--- src/modulo2/atividadep59/atp59/src/main/java/com/atp/App.java
+++ src/modulo2/atividadep59/atp59/src/main/java/com/atp/App.java
@@ -1,13 +1,42 @@
 package com.atp;
 
-/**
- * Hello world!
- *
- */
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.atp.atp59.model.Categoria;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("== SISTEMA DE BANCO COM JPA ==");
+        
+        // inicializando e atribuindo valores ao model a ser adicionado ao BD
+        Categoria model = new Categoria();
+        model.setNome("Teste2 JPA no Maven");
+        model.setDescricao("Vem aí menos dor de cabeça, quem amou?");
+
+        // name definido no arquivo persistence.xml
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("banco");
+        EntityManager em = factory.createEntityManager();
+
+        //fazendo uma insercao no BD
+        
+        //inicia a transacao
+        em.getTransaction().begin();
+        // faz a persistencia (o insert)
+        em.persist(model);
+        // realiza a transacao
+        em.getTransaction().commit();
+
+        // faz a busca e atribui uma lista do tipo categoria
+        List<Categoria> lista = em.createQuery("SELECT c FROM Categoria c", Categoria.class).getResultList();
+
+        for (Categoria categoria : lista) {
+            System.out.printf("ID: %d - NOME: %s - DESCRICAO: %s\n",categoria.getId(),categoria.getNome(),categoria.getDescricao());
+        }
     }
 }
