ff666446f92dea156aeb1a452b1830c60647758e
lorenascimentoo <lorenanascimento28@gmail.com>
2021-11-29 14:26:45 -0300

    implementacao do create e read de cliente

--- src/modulo2/atividadep64/atp64/src/main/java/com/atp/atp64/controller/ClienteController.java
+++ src/modulo2/atividadep64/atp64/src/main/java/com/atp/atp64/controller/ClienteController.java
@@ -1,11 +1,43 @@
 package com.atp.atp64.controller;
 
+import java.util.List;
+
+import com.atp.atp64.model.Cliente;
 import com.atp.atp64.repository.ClienteRepository;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class ClienteController {
+
     private ClienteRepository cRepository;
 
     public ClienteController(ClienteRepository rep) {
         this.cRepository = rep;
     }
+
+    @PostMapping("filme/salvar") //endpoint para salvar
+    public String create(Cliente model){
+        cRepository.save(model);
+        return "redirect:/cliente";
+    }
+
+    @GetMapping("cliente") //endpoint para listar
+    public String read(Model req){
+        List<Cliente> lista = (List<Cliente>)cRepository.findAll();
+        req.addAttribute("clientes", lista);
+        return "clientes";
+    }
+
+    
+    @GetMapping("cliente/form")//endpoint para exibir o formulario
+    public String formulario(){
+        return "cliente-form";
+    }
+
+
+
 }
