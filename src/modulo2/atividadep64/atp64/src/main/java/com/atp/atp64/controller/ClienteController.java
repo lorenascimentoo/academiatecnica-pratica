cee384046377e4ed440eecf4745ddccb40d90960
lorenascimentoo <lorenanascimento28@gmail.com>
2021-11-29 14:56:14 -0300

    Update e Delete de cliente

--- src/modulo2/atividadep64/atp64/src/main/java/com/atp/atp64/controller/ClienteController.java
+++ src/modulo2/atividadep64/atp64/src/main/java/com/atp/atp64/controller/ClienteController.java
@@ -8,6 +8,7 @@ import com.atp.atp64.repository.ClienteRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -19,25 +20,39 @@ public class ClienteController {
         this.cRepository = rep;
     }
 
-    @PostMapping("filme/salvar") //endpoint para salvar
+    @PostMapping("/cliente/salvar") //endpoint para salvar
     public String create(Cliente model){
         cRepository.save(model);
         return "redirect:/cliente";
     }
 
-    @GetMapping("cliente") //endpoint para listar
+    @GetMapping("/cliente") //endpoint para listar
     public String read(Model req){
         List<Cliente> lista = (List<Cliente>)cRepository.findAll();
         req.addAttribute("clientes", lista);
         return "clientes";
     }
 
-    
-    @GetMapping("cliente/form")//endpoint para exibir o formulario
-    public String formulario(){
+    @GetMapping("/cliente/editar/{id}")//endpoint para delecao
+    // o metodo recebe o valor do endpoint passado como parametro
+    public String update(@PathVariable int id,Model req){
+        Cliente model = cRepository.findById(id).get();
+        req.addAttribute("cliente", model);
         return "cliente-form";
     }
 
+    @GetMapping("/cliente/deletar/{id}")//endpoint para delecao
+    // o metodo recebe o valor do endpoint passado como parametro
+    public String delete(@PathVariable int id){
+        cRepository.deleteById(id);
+        return "redirect:/cliente";
+    }
 
+    @GetMapping("/cliente/cadastrar")//endpoint para exibir o formulario
+    public String formulario(Model req){
+        Cliente model = new Cliente();
+        req.addAttribute("cliente", model);
+        return "cliente-form";
+    }
 
 }
